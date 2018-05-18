bffd6e3ca6af03f13bdf6841498f8352b4d648ac
alyciamriley <alyciarileym@gmail.com>
2018-05-18 14:52:37 -0500

    very basic layout and user story.

---
+++ assets/js/logic.js
@@ -0,0 +1,27 @@
+//App loads and displays 4 characters (populated from JSON file?)
+
+//Player chooses character by clicking fighters picture
+
+
+//That fighter moves to "your character" div #yours
+
+//All other characters move to #enemies div
+
+//Player chooses a opponent. Opponent moves to #defender div
+
+//Player can click attack btton #attack.  
+
+//When player clicks attack button, their character damages defender.  
+
+//Opponent loses health points.  Points are displayed at bottom of defenders picture 
+
+//Opponent counters attack - players character loses HP.  Points show up at bottom of characters picture.
+
+//When opponent's HP gets to 0, opponent disappears (hide feature?)
+
+//Player chooses new opponent, steps repeat
+
+//If character defeats all enemies, user wins.
+
+//if character reaches 0 HP, user loses. 
+
